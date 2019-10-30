121bb2aeb25081e8dc3d3fef36da1b9ea5a9bb99
Alex <zzheads@gmail.com>
2019-10-31 01:02:48 +0400

    Initial Commit

---
+++ IntTestTests/IntTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  IntTestTests.swift
+//  IntTestTests
+//
+//  Created by Алексей Папин on 31.10.2019.
+//  Copyright © 2019 Алексей Папин. All rights reserved.
+//
+
+import XCTest
+@testable import IntTest
+
+class IntTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
